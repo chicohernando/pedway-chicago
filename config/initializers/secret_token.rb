192992261815830fb24e53fbbbc970b950af92f0
Carissa Niederer <carissa.niederer@recsolu.com>
2013-02-05 18:08:26 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PedwayChicago::Application.config.secret_token = '6fe4cf0064d52e1aa3e477321cfb7ac91de00cd616fe06a03d4e2eb14adfa7833836e08cd74611cb5d381227082cd54149b307d4a0ce6796329134e200041f5e'

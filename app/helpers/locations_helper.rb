1edce4b750c5623adffbc39519c42ec81abf04d2
Carissa Niederer <cniederer@gmail.com>
2013-02-14 22:24:41 -0600

    made a locations helper for static google maps

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,2 +1,7 @@
 module LocationsHelper
+
+  def static_map(latitude, longitude)
+    image_tag("https://maps.googleapis.com/maps/api/staticmap?center=#{latitude},#{longitude}&zoom=15&size=300x300&sensor=false&markers=color:green|#{latitude},#{longitude}")
+  end
+
 end
